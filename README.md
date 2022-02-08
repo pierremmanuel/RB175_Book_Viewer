b1808c9c02835a3bc4ef7639ada320ba691bbafa
Pierre-Emmanuel Roussel <pierre.emmanuel.rssl@gmail.com>
2022-02-07 19:17:07 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Building a small reading application with Sinatra
+
+In this lesson, we're going to be working with the Sinatra web development framework. Sinatra, like all good web development frameworks, is a productivity tool meant to help web developers speed up development by automating common tasks. This is powerful for the experienced, but confusing for the inexperienced.
+
+We'll be building a small mobile-friendly ebook reading application using Sinatra. As we work through the assignments, we'll learn about the basics of Sinatra and see how it differs from handling responses manually.
---
+++ book_viewer.rb
@@ -0,0 +1,106 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "yaml"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+  @users = YAML.load_file('users.yaml')
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/" do
+  @words = ["blubber", "beluga", "galoshes", "mukluk", "narwhal"]
+  erb :index
+end
+
+get "/chapters/:number" do # :number is a parameter that represents any single segment that follows '/chapters'
+  number = params[:number].to_i # params is a hash automatically available in routes
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+get "/users" do
+  erb :users
+end
+
+get "/:user_name" do
+  @user_name = params[:user_name].to_sym
+
+  redirect "/" unless @users.keys.include?(@user_name)
+
+  @email = @users[@user_name][:email]
+  @interests = @users[@user_name][:interests]
+
+  erb :user
+end
+
+not_found do
+  redirect "/"
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map.with_index do |paragraph, index|
+      "<p id=paragraph#{index}>#{paragraph}</p>"
+    end.join
+  end
+
+  def highlight(text, term)
+    text.gsub(term, "<strong>#{term}</strong>")
+  end
+
+  def count_interests(users)
+    users.reduce(0) do |sum, (name, user)|
+      sum + user[:interests].size
+    end
+  end
+end
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
